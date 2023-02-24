c8b4e3db3254291b8473cfed7fbc83073ec1bc44
Fathan Fisabililah <fathanrbe@gmail.com>
2023-02-24 20:27:37 +0700

    Awesome Design feature

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import navigation from './scripts/theme/navigation'
+import works from './scripts/theme/works'
+import testimonials from './scripts/theme/testimonials'
+navigation();
+works();
+testimonials();
